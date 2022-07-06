c6bdae3a2c4506740ce34aed7c422d7157f5418f
Victor Barbosa <vicldb99@gmail.com>
2022-07-06 02:02:32 -0300

    webpack fast refresh

--- chapter-i/src/components/Counter.jsx
+++ chapter-i/src/components/Counter.jsx
@@ -11,7 +11,7 @@ export const Counter = () => {
     <div>
       <h2>{counter}</h2>
       <button type="button" onClick={increment}>
-        Increment
+        Increment yeah
       </button>
     </div>
   );
